f62c7e8bff8dee0a33f3d781b085bd54aa02fc52
Zeetheer <81860686+Zeetheer@users.noreply.github.com>
2024-05-22 20:04:28 +0900

    doc comment

--- lib/src/eng_to_kor.dart
+++ lib/src/eng_to_kor.dart
@@ -3,6 +3,10 @@ import 'package:korea_regexp/src/implode.dart';
 
 final enToKr = { for (var e in keys) e.last : e.first };
 
+///English typing -> Korean typing
+///영티 -> 한타
+
+/// "dkssud" -> "안녕"
 String engToKor(String eng) {
   final kor = eng.split('').map((char) => enToKr[char] ?? char).join();
   return implode(kor);
