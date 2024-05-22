b686fc8076c8eb39b416575c6508cca4d3ca9f35
Zeetheer <81860686+Zeetheer@users.noreply.github.com>
2024-05-22 21:18:16 +0900

    doc update

--- lib/src/kor_to_eng.dart
+++ lib/src/kor_to_eng.dart
@@ -4,6 +4,10 @@ import 'package:korea_regexp/src/explode.dart';
 Map<String, String> KR_TO_EN =
     Map.fromIterables(keys.map((e) => e[0]), keys.map((e) => e[1]));
 
+///Korean typing -> English typing
+///힌타 -> 영타
+
+/// '안녕' -> 'dkssud'
 korToEng(String text) {
   return text
       .split('')
